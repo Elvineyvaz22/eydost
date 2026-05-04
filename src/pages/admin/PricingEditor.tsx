9d06af3069bff26602ac0f217a842503e76145ec
Elvineyvaz22 <64049604+Elvineyvaz22@users.noreply.github.com>
2026-05-04 16:24:38 +0400

    Fix syntax error in PricingEditor.tsx

--- src/pages/admin/PricingEditor.tsx
+++ src/pages/admin/PricingEditor.tsx
@@ -602,6 +602,7 @@ export default function PricingEditor() {
             </table>
           </div>
         </div>
+        )}
 
         {showForm && (
           <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center p-4">
